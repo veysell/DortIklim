8f4188d2ce7c1569a5e551d62bca0e9c57b91ae0
veysell <vdrn76@gmail.com>
2024-02-03 03:30:19 +0300

    session add

--- admin/login.php
+++ admin/login.php
@@ -1,79 +1,118 @@
 <!DOCTYPE html>
 <html lang="tr">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Corona Admin</title>
-    <!-- plugins:css -->
-    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
-    <!-- Layout styles -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- End layout styles -->
-    <link rel="shortcut icon" href="assets/images/favicon.png" />
-  </head>
-  <body>
-    <div class="container-scroller">
-      <div class="container-fluid page-body-wrapper full-page-wrapper">
-        <div class="row w-100 m-0">
-          <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
-            <div class="card col-lg-4 mx-auto">
-              <div class="card-body px-5 py-5">
-                <h3 class="card-title text-left mb-3">Login</h3>
-                <form>
-                  <div class="form-group">
-                    <label>Username or email *</label>
-                    <input type="text" class="form-control p_input">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Corona Admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- End layout styles -->
+  <link rel="shortcut icon" href="assets/images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="row w-100 m-0">
+        <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
+          <div class="card col-lg-4 mx-auto">
+            <div class="card-body px-5 py-5">
+              <h3 class="card-title text-left mb-3">Login</h3>
+              <?php
+              session_start();
+              include("database/connection.php");
+              if (isset($_SESSION["Oturum"]) && $_SESSION["Oturum"] == "6789") {
+                header("location:index");
+              }
+
+              ?>
+              <form method="post" action="login">
+                <div class="form-group">
+                  <label>Username or email *</label>
+                  <input type="text" name="txtUserName" class="form-control p_input">
+                </div>
+                <div class="form-group">
+                  <label>Password *</label>
+                  <input type="password" name="txtUserPass" class="form-control p_input">
+                </div>
+                <div class="form-group d-flex align-items-center justify-content-between">
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="checkbox" class="form-check-input"> Remember me </label>
                   </div>
-                  <div class="form-group">
-                    <label>Password *</label>
-                    <input type="text" class="form-control p_input">
-                  </div>
-                  <div class="form-group d-flex align-items-center justify-content-between">
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input"> Remember me </label>
-                    </div>
-                    <a href="#" class="forgot-pass">Forgot password</a>
-                  </div>
-                  <div class="text-center">
-                    <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
-                  </div>
-                  <div class="d-flex">
-                    <button class="btn btn-facebook mr-2 col">
-                      <i class="mdi mdi-facebook"></i> Facebook </button>
-                    <button class="btn btn-google col">
-                      <i class="mdi mdi-google-plus"></i> Google plus </button>
-                  </div>
-                  <p class="sign-up">Don't have an Account?<a href="#"> Sign Up</a></p>
-                </form>
-              </div>
+                  <a href="#" class="forgot-pass">Forgot password</a>
+                </div>
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
+                </div>
+                <div class="d-flex">
+                  <button class="btn btn-facebook mr-2 col">
+                    <i class="mdi mdi-facebook"></i> Facebook </button>
+                  <button class="btn btn-google col">
+                    <i class="mdi mdi-google-plus"></i> Google plus </button>
+                </div>
+                <p class="sign-up">Don't have an Account?<a href="#"> Sign Up</a></p>
+              </form>
+              <?php
+
+
+              if ($_POST) {
+                $name = $_POST["txtUserName"];
+                $pass = $_POST["txtUserPass"];
+                echo "name:" . ($name) . $pass;
+                $sorgu = $baglanti->prepare("select * from user where Name=:userName");
+                $sorgu->execute(['userName' => htmlspecialchars($name)]);
+                $sonuc = $sorgu->fetch();
+                
+                if ($sonuc!=null){
+                  if(($pass==$sonuc['Password'])){
+                      $_SESSION["Oturum"] = 6789;
+                      $_SESSION["usernaname"] = $username;
+                      $_SESSION["role"] = $sonuc["role"];
+                      header("location:index");
+                  }
+                  else{
+                      echo "Kullanıcı Adı veya Şifre Hatalı";
+                  }
+                 }
+                 else{
+                  echo "Kayıt Bulunamadı";
+                 }
+              }
+
+              ?>
             </div>
           </div>
-          <!-- content-wrapper ends -->
         </div>
-        <!-- row ends -->
+        <!-- content-wrapper ends -->
       </div>
-      <!-- page-body-wrapper ends -->
+      <!-- row ends -->
     </div>
-    <!-- container-scroller -->
-    <!-- plugins:js -->
-    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
-    <script src="assets/js/off-canvas.js"></script>
-    <script src="assets/js/hoverable-collapse.js"></script>
-    <script src="assets/js/misc.js"></script>
-    <script src="assets/js/settings.js"></script>
-    <script src="assets/js/todolist.js"></script>
-    <!-- endinject -->
-  </body>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="assets/js/off-canvas.js"></script>
+  <script src="assets/js/hoverable-collapse.js"></script>
+  <script src="assets/js/misc.js"></script>
+  <script src="assets/js/settings.js"></script>
+  <script src="assets/js/todolist.js"></script>
+  <!-- endinject -->
+</body>
+
 </html>
---
+++ admin/queries/queries.php
@@ -0,0 +1,71 @@
+<?php
+// admin/inc/queries.php
+
+include_once('database/connection.php');
+
+function getAllData() {
+    global $conn;
+
+    $sql = "SELECT * FROM tablo_adı";
+    $result = $conn->query($sql);
+
+    $data = array();
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    }
+
+    return $data;
+}
+
+function insertData($data) {
+    global $conn;
+
+    $column1 = $data['column1'];
+    $column2 = $data['column2'];
+    // Diğer sütunları ekleyin...
+
+    $sql = "INSERT INTO tablo_adı (column1, column2) VALUES ('$column1', '$column2')";
+    
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkUser($userName, $password) {
+    global $conn;
+    echo $conn;
+    echo $userName."--".$password."";
+    // Önce kullanıcının adını kullanarak veritabanından bilgileri çekelim
+    $stmt = $conn->prepare("SELECT * FROM user WHERE Name = ?");
+    $stmt->bind_param("s", $userName);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if ($result->num_rows > 0) {
+        // Kullanıcı adı doğru, şimdi şifreyi kontrol edelim
+        $user = $result->fetch_assoc();
+
+        if (password_verify($password, $user['userPass'])) {
+            // Şifre doğru, kullanıcı girişini başlat
+            session_start();
+            $_SESSION["Oturum"] = 6789;
+            $_SESSION["username"] = $userName;
+            $_SESSION["role"] = $user["role"];
+            header("location: index");
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    $stmt->close();
+}
+
+// Diğer CRUD işlemleri için fonksiyonları ekleyebilirsiniz.
+
